a3fdae57f7a439815f99626ee975fea0ce6fd039
LesLivia <livia.lestingi@polimi.it>
2023-04-01 08:22:48 +0000

    merging

--- errors_DP1.sh
+++ errors_DP1.sh
@@ -2,3 +2,7 @@ python3 src/main.py DP1_crit
 python3 src/main.py DP1_stable
 python3 src/main.py DP1_inatt
 python3 src/main.py DP1_foc
+python3 src/main.py DP1_busy
+python3 src/main.py DP1_ava
+python3 src/main.py DP1_inex
+python3 src/main.py DP1_exp
